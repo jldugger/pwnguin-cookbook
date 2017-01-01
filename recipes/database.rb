c56fcf488e6ec710ea2be03435b15264d0b7c42e
Justin Dugger <jldugger@osuosl.org>
2017-01-01 11:44:15 -0800

    fix rubocop issues

--- recipes/database.rb
+++ recipes/database.rb
@@ -9,6 +9,6 @@ include_recipe 'postgresql::server'
 node.set['postgresql']['config_pgtune']['db_type'] = 'web'
 
 memory = node['memory']['total'].split('kB')[0].to_i
-node.set['postgresql']['config_pgtune']['total_memory'] = (memory / 2).floor.to_s + "kB"
+node.set['postgresql']['config_pgtune']['total_memory'] = (memory / 2).floor.to_s + 'kB'
 
-#include_recipe 'postgresql::config_pgtune'
+# include_recipe 'postgresql::config_pgtune'
