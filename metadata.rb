4d95c923e382a214db5f951cbd81ab398eabfca4
Justin Dugger <jldugger@osuosl.org>
2017-01-08 20:41:45 -0800

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jldugger@gmail.com'
 license 'MIT'
 description 'Installs/Configures pwnguin.net'
 long_description 'Installs/Configures pwnguin.net'
-version '0.4.2'
+version '0.4.3'
 
 issues_url 'https://github.com/jldugger/pwnguin/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/jldugger/pwnguin' if respond_to?(:source_url)
