5071d586ef97fc998d6ec23c074c12eb6768da74
Justin Dugger <jldugger@gmail.com>
2017-08-02 16:37:29 -0700

    move shell server related resources to new recipe

---
+++ recipes/shell.rb
@@ -0,0 +1,29 @@
+#
+# Cookbook Name:: pwnguin
+# Recipe:: shell
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+
+
+%w(screen git ack-grep colordiff fail2ban irssi moreutils
+   myrepos pastebinit pwgen pv vim).each do |pkg|
+  package pkg do
+    action :upgrade
+  end
+end
+
+user 'jldugger' do
+  home '/home/jldugger'
+  shell '/bin/bash'
+  manage_home true
+end
+
+group 'sudo' do
+  action :modify
+  members ['jldugger']
+  append true
+end
+
+user 'ubuntu' do
+  action :remove
+end
