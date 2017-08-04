f0f5d1d391dad8d2a46b4713e506d37b9c9c5f2f
Justin Dugger <jldugger@gmail.com>
2017-08-03 18:52:24 -0700

    fix food critic

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,13 @@
 name 'pwnguin'
 maintainer 'Justin Dugger'
 maintainer_email 'jldugger@gmail.com'
-license 'MIT'
+license 'GPL-2.0'
 description 'Installs/Configures pwnguin.net'
 long_description 'Installs/Configures pwnguin.net'
 version '1.1.3'
+
+chef_version '>= 12.5' if respond_to?(:chef_version)
+supports 'ubuntu'
 
 issues_url 'https://github.com/jldugger/pwnguin/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/jldugger/pwnguin' if respond_to?(:source_url)
