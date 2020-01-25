3d61d46039cd6da573af75c1044024d6c1d7e864
Justin Dugger <jldugger@gmail.com>
2020-01-24 19:14:42 -0800

    extract base cookbook settings from default

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,16 +4,6 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-node.default['chef_client']['cron']['hour'] = '*'
-node.default['chef_client']['cron']['minute'] = '*/30'
-node.default['chef_client']['cron']['mailto'] = 'jldugger+chef@gmail.com'
-node.default['chef_client']['cron']['use_cron_d'] = true
-node.default['chef_client']['cron']['log_file'] = '/var/log/chef/client.log'
-node.default['chef_client']['init_style'] = 'none'
-
-include_recipe 'chef-client::cron'
-include_recipe 'chef-client::delete_validation'
-
 %w[htop httping mtr-tiny sslscan traceroute whois].each do |pkg|
   package pkg do
     action :upgrade
